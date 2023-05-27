0ed5b3d3859387a8846120eb07215e5dd7596791
Daniel Amoruso <damoruso321@gmail.com>
2023-05-27 19:21:06 -0400

    Commit #3
    Added command for resetting level and stats.

--- src/main/java/me/damor/statplugin/StatPlugin.java
+++ src/main/java/me/damor/statplugin/StatPlugin.java
@@ -1,6 +1,7 @@
 package me.damor.statplugin;
 
 import me.damor.statplugin.commands.StatGetCommand;
+import me.damor.statplugin.commands.StatResetCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class StatPlugin extends JavaPlugin {
@@ -26,6 +27,7 @@ public final class StatPlugin extends JavaPlugin {
 
         // Register commands
         getCommand("statget").setExecutor(new StatGetCommand(playerManager));
+        getCommand("statreset").setExecutor(new StatResetCommand(playerManager));
     }
 
     @Override
---
+++ src/main/java/me/damor/statplugin/commands/StatResetCommand.java
@@ -0,0 +1,34 @@
+package me.damor.statplugin.commands;
+
+import me.damor.statplugin.PlayerManager;
+import me.damor.statplugin.PlayerStat;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class StatResetCommand implements CommandExecutor {
+    private PlayerManager playerManager;
+
+    public StatResetCommand(PlayerManager playerManager){
+        this.playerManager = playerManager;
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        Player player = (Player)commandSender;
+        PlayerStat playerStat = playerManager.getPlayerStat(player);
+
+        playerStat.levelOverall = 1;
+        playerStat.xp = 0;
+        playerStat.luckLevel = 0;
+        playerStat.swiftnessLevel = 0;
+        playerStat.strengthLevel = 0;
+
+        player.sendMessage(ChatColor.YELLOW + "Level and stats have been reset! Good luck starting fresh!");
+
+        return true;
+    }
+}
